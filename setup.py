c84816ebd16284659817b2d6197f0e18cd9a40f4
kunalshelke90 <shelkekunal90@gmail.com>
2024-09-19 23:53:57 +0530

    installation of lib. done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.1',
+    author= 'Kunal Shelke',
+    author_email= 'shelkekunal90@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
